3b6949d83c875a11228bb86f5cc516c493a0665d
EZEN-135 <EZEN-135@DESKTOP-EFOC4DA>
2022-07-13 15:58:34 +0900

    NKH

---
+++ NKH/src/NKH.java
@@ -0,0 +1,16 @@
+
+public class NKH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("PSU " + " OGH " + " KMJ");
+		System.out.println("KSJ " + " KJS " + " KTH");
+		System.out.println("KDH " + " PYM " + " SDY");
+		System.out.println("ABK " + " CEJ " + " LYB");
+		System.out.println("LJH " + " CYS " + " CHS");
+		System.out.println("HWJ");
+		
+	}
+
+}
